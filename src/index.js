546e58e5588830c73da2fa4b6732e97dbecc9ff8
valor323 <caine101@mail.chapman.edu>
2018-09-17 14:00:58 -0700

    craeted greeting function  with a lucky number function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+function luckyNumber(){
+    let luckyNumber = Math.ceil(Math.random()*1000)
+    return luckyNumber;
+}
+
+function greeting(user){
+    return <h1 class='container' >
+        <h1>greeting's to you {user.name}</h1>
+        <h2 class='text-muted' >your lucky number is {user.luckyNumber} </h2>
+    </h1>
+}
+
+
+var user = {
+    name:'chase',
+    luckyNumber: luckyNumber()
+}
+
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+   greeting(user),
     document.getElementById('root')
 );
+
