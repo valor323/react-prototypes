2a2b87c4bb1d4472baeee419d33d19bb3b452d56
valor323 <caine101@mail.chapman.edu>
2018-09-17 15:55:22 -0700

    finished Prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,31 @@
 
-
 import React from 'react';
+import Table from './table';
+
+
+const students = [
+    {
+        Name : 'Chase',
+        Course: 'Science',
+        Garde: 65
+    },
+    {
+        Name: 'Sean',
+        Course: 'English',
+        Grade: 95
+    },
+    {
+        Name: 'Kareem',
+        Course: 'Engineering',
+        Grade: 0
+    },
+]
 
 export default () => {
-    return <h1>My first React functional component</h1>
+    return(
+   <div className="container">
+   <h1>Student Grade Table</h1>
+   <Table data={students}/>
+   </div>
+    )
 }
--- src/components/table.js
+++ src/components/table.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
-export default () => {
-    return <
-}
+export default (props) => {
+
+    const tableRows = props.data.map((items, index) => {
+        return(
+            <tr key={index}>
+                <td>{items.Name}</td>
+                <td>{items.Course}</td>
+                <td>{items.Grade}</td>
+            </tr>
+        )
+    });
+    return (
+        <table className='table'>
+             <thead className ='thead-inverse'>
+                <tr>
+                    <th>Name</th>
+                    <th>Course</th>
+                    <th>Grade</th>
+                </tr>
+            </thead>
+            <tbody>
+              {tableRows}
+            </tbody>
+        </table>
+        )
+        }
+
